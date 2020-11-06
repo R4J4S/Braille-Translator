65bd70452b6f1cab73b1170b02b4a0ecabe6a907
Rajas <rs.k.mahadule@gmail.com>
2020-11-06 13:44:08 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@ int main()
   //prompting and taking input sentence from user.
   cout<<"Given Sentence:"<<endl;
   string sentence = "I have 2 dogs. A pomeranian named \"DODO\" and labrador named \"RIO\". Dodo is 9 years old and Rio is 10 months old.";
-  cout<<sentence<<endl;
   
+  cout<<sentence<<endl;
+
   cout<<"BRAILLE SENTENCE:"<<endl;
 
   //calling function toBraille from letterstoBraille.cpp
